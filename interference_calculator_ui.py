bfdcb63f71ce5378de4ad0e3943af7028dc93453
zanpeeters <zan@asiaa.sinica.edu.tw>
2017-05-06 17:33:49 +0800

    Major overhaul of GUI: now works with PyQt5, no longer uses QWebKit and a HTML table to display data, uses QTableView instead.

--- interference_calculator_ui.py
+++ interference_calculator_ui.py
@@ -2,21 +2,128 @@
 # -*- coding: utf-8 -*-
 """ GUI for mass spectrum program. """
 
-from PyQt4 import QtCore, QtGui, QtWebKit
-from pandas import set_option
-import sys, re
-import masstable
-import sorttable
+try:
+    from PyQt5 import QtCore, QtGui
+    from PyQt5 import QtWidgets as widgets
+except ImportError:
+    try:
+        from PyQt4 import QtCore, QtGui
+        from PyQt4 import QtGui as widgets
+    except ImportError:
+        raise ImportError('You need to have either PyQt4 or PyQt5 installed.')
 
-# Debug: trace warnings
-# import warnings
-# warnings.filterwarnings('error')
+from pandas import DataFrame
+import sys, re, platform
+import masstable
 
 _float_qrx = QtCore.QRegExp(r'^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$')
 _float_validator = QtGui.QRegExpValidator(_float_qrx)
+_isotope_rx = re.compile(r'(\d*[A-Z][a-z]{0,2})')
+_charges_rx = re.compile(r'(\d+)')
+
+class TableModel(QtCore.QAbstractTableModel):
+    """ Take a pandas DataFrame and set data in a QTableModel (read-only). """
+
+    def __init__(self, data, parent=None):
+        QtCore.QAbstractTableModel.__init__(self, parent)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parent=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if index.isValid():
+            if role == QtCore.Qt.DisplayRole:
+                if index.column() in (1, 2):
+                    return '{:.5f}'.format(self._data.iloc[index.row(), index.column()])
+                elif index.column() == 3:
+                    return '{:.1f}'.format(self._data.iloc[index.row(), index.column()])
+                elif index.column() == 4:
+                    return '{:.5g}'.format(self._data.iloc[index.row(), index.column()])
+                else:
+                    return '{}'.format(self._data.iloc[index.row(), index.column()])
+            elif role == QtCore.Qt.TextAlignmentRole:
+                if index.column() == 0:
+                    return QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter
+                else:
+                    return QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter
+
+    def headerData(self, rowcol, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return self._data.columns[rowcol]
+        if orientation == QtCore.Qt.Vertical and role == QtCore.Qt.DisplayRole:
+            return self._data.index[rowcol]
+
+    def sort(self, column, order):
+        # QtCore.Qt.AscendingOrder = 0
+        # QtCore.Qt.DescendingOrder = 1
+        ascending = not bool(order)
+        colname = self._data.columns[column]
+        self._data.sort_values(colname, ascending=ascending, inplace=True)
+        self.beginResetModel()
+        self.endResetModel()
 
 
-class LogSpinBox(QtGui.QDoubleSpinBox):
+class TableView(widgets.QTableView):
+    """ Implement a QTableView which can display HTML in arbitrary columns """
+    def __init__(self, html_cols=None, parent=None):
+        widgets.QTableView.__init__(self, parent=parent)
+        self.setSortingEnabled(True)
+        if html_cols is not None:
+            if isinstance(html_cols, int):
+                html_cols = [html_cols]
+            [self.setItemDelegateForColumn(c, HTMLDelegate(parent=parent)) for c in html_cols]
+
+
+class HTMLDelegate(widgets.QStyledItemDelegate):
+    """ Display HTML in a table cell. """
+    def __init__(self, parent=None):
+        widgets.QStyledItemDelegate.__init__(self, parent=parent)
+
+    def createEditor(self, parent, option, index):
+        """ disable editing """
+        return None
+
+    def paint(self, painter, option, index):
+        """ paint QTextDocument """
+        options = widgets.QStyleOptionViewItem(option)
+        self.initStyleOption(options, index)
+
+        if options.widget:
+            style = options.widget.style()
+        else:
+            style = widgets.QApplication.style()
+
+        textbox = QtGui.QTextDocument()
+        textbox.setHtml(options.text)
+        textbox.setTextWidth(options.rect.width())
+        options.text = ''
+        style.drawControl(widgets.QStyle.CE_ItemViewItem, options, painter)
+        context = QtGui.QAbstractTextDocumentLayout.PaintContext()
+        textrect = style.subElementRect(widgets.QStyle.SE_ItemViewItemText, options)
+
+        painter.save()
+        painter.translate(textrect.topLeft())
+        painter.setClipRect(textrect.translated(-textrect.topLeft()))
+        textbox.documentLayout().draw(painter, context)
+        painter.restore()
+
+    def sizeHint(self, option, index):
+        """ Set size hint for HTMLDelegate. """
+        options = widgets.QStyleOptionViewItem(option)
+        self.initStyleOption(options, index)
+
+        textbox = QtGui.QTextDocument()
+        textbox.setHtml(options.text)
+        textbox.setTextWidth(options.rect.width())
+
+        return QtCore.QSize(textbox.idealWidth(), textbox.size().height())
+
+
+class LogSpinBox(widgets.QDoubleSpinBox):
     """ Like QDoubleSpinBox but steps by powers of 10 
 
         Default minimum and maximum are 10^-99 and 10^+99,
@@ -47,48 +154,20 @@ class LogSpinBox(QtGui.QDoubleSpinBox):
         return '{:n}'.format(value)
 
 
-class MainWindow(QtGui.QMainWindow):
+class MainWindow(widgets.QMainWindow):
     """ Main window for masstable ui. """
     def __init__(self):
         super().__init__()
-
-        # Setup
         self.setWindowTitle('Mass interference calculator')
         self.resize(600, 720)
         self.setMinimumSize(600, 720)
         self.setCentralWidget(MainWidget(parent=self))
 
-        # Menubar
-        # For Mac OSX:
-        # - preferences and quit will automatically be moved to Application
-        #   menu where they belong.
-        # - File menu is then empty and will not be rendered.
-        # - On pyqt, name of the application is Python and can only be changed when
-        #   installed as an .app with name in Info.plist.
-        # - Quit menu always exists in Application menu.
-        # - Ctrl is mapped to Cmd on Mac.
-        # self.preferences_menuitem = QtGui.QAction('Preferences', self)
-        # self.preferences_menuitem.setShortcut('Ctrl+.')
-        # self.preferences_menuitem.triggered.connect(self.open_preferences)
-        # 
-        # self.quit_menuitem = QtGui.QAction('&Quit', self)
-        # self.quit_menuitem.setShortcut('Ctrl+Q')
-        # self.quit_menuitem.triggered.connect(QtGui.qApp.quit)
-        # 
-        # self.menubar = self.menuBar()
-        # self.file_menu = self.menubar.addMenu('&Program')
-        # self.file_menu.addAction(self.preferences_menuitem)
-        # self.file_menu.addAction(self.quit_menuitem)
 
-
-# class PreferencesWindow(QtGui.QWidget):
-
-
-class MainWidget(QtGui.QWidget):
+class MainWidget(widgets.QWidget):
     """ Central widget class for masstable ui. """
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-        # Default values
         self.atoms = []
         self.charges = [1]
         self.mz = 0.0
@@ -98,82 +177,83 @@ class MainWidget(QtGui.QWidget):
         self.has_minabundance = False
 
         # Inputs
-        # self.atoms_label = QtGui.QLabel('sample composition', parent=self)
-        self.atoms_input = QtGui.QLineEdit(parent=self)
+        self.atoms_input = widgets.QLineEdit(parent=self)
         self.atoms_input.setPlaceholderText('list of atoms in sample: C H Si Ca')
 
-        self.maxsize_label = QtGui.QLabel('max size', parent=self)
-        self.maxsize_input = QtGui.QSpinBox(parent=self)
+        self.maxsize_label = widgets.QLabel('max size', parent=self)
+        self.maxsize_input = widgets.QSpinBox(parent=self)
         self.maxsize_input.setValue(self.maxsize)
 
-        self.charges_label = QtGui.QLabel('target charge(s)', parent=self)
-        self.charges_input = QtGui.QLineEdit(' '.join([str(c) for c in self.charges]), parent=self)
+        self.charges_label = widgets.QLabel('target charge(s)', parent=self)
+        self.charges_input = widgets.QLineEdit(' '.join([str(c) for c in self.charges]), parent=self)
 
-        self.chargesign_input = QtGui.QComboBox(parent=self)
+        self.chargesign_input = widgets.QComboBox(parent=self)
         self.chargesign_input.addItem('+', '+')
         self.chargesign_input.addItem('-', '-')
 
-        self.mz_label = QtGui.QLabel('target mass/charge', parent=self)
-        self.mz_input = QtGui.QLineEdit(parent=self)
+        self.mz_label = widgets.QLabel('target mass/charge', parent=self)
+        self.mz_input = widgets.QLineEdit(parent=self)
         self.mz_input.setPlaceholderText('number or formula: 26.0 or 12C 14N')
 
-        self.mzrange_label = QtGui.QLabel('±', parent=self)
-        self.mzrange_input = QtGui.QDoubleSpinBox(parent=self)
+        self.mzrange_label = widgets.QLabel('±', parent=self)
+        self.mzrange_input = widgets.QDoubleSpinBox(parent=self)
         self.mzrange_input.setValue(self.mzrange)
         self.mzrange_input.setSingleStep(0.1)
         self.mzrange_input.setDecimals(3)
 
-        self.minabundance_label = QtGui.QLabel('missing abundance value', parent=self)
+        self.minabundance_label = widgets.QLabel('missing abundance value', parent=self)
         self.minabundance_input = LogSpinBox(parent=self)
         self.minabundance_input.setValue(self.minabundance)
         self.minabundance_input.setEnabled(self.has_minabundance)
-        self.minabundance_select = QtGui.QCheckBox()
+        self.minabundance_select = widgets.QCheckBox()
         self.minabundance_select.setChecked(self.has_minabundance)
 
         # Action button
-        self.interference_button = QtGui.QPushButton('calculate interference', parent=self)
-        self.isotopes_button = QtGui.QPushButton('natural abundance', parent=self)
+        self.interference_button = widgets.QPushButton('calculate interference', parent=self)
+        self.isotopes_button = widgets.QPushButton('natural abundance', parent=self)
 
         # Table output
-        self.table_output = QtWebKit.QWebView()
+        self.table_output = TableView(html_cols=0)
 
-        # Qt4 on Mac has a problem with the focus rectangle around input
-        # boxes. Not fixed until Qt5.
-        self.atoms_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
-        self.mz_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
-        self.mzrange_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
-        self.charges_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
-        self.maxsize_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
-        self.minabundance_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
-        self.table_output.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
+        # Qt4 on Mac Snow Leopard and older has a problem with the focus rectangle
+        # around input boxes. (error: unlockFocus called too many times)
+        if sys.platform == 'darwin':
+            if int(platform.mac_ver()[0].split('.')[1]) < 7 and int(QtCore.QT_VERSION) < 0x50000:
+                self.atoms_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
+                self.mz_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
+                self.mzrange_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
+                self.charges_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
+                self.maxsize_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
+                self.minabundance_input.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
+                self.table_output.setAttribute(QtCore.Qt.WA_MacShowFocusRect, on=False)
 
         # Show input errors on statusbar
         self.statusbar = self.parent().statusBar()
         self.statusbar.setStyleSheet('color: #FF0000;')
 
         # Layout objects
-        pol = QtGui.QSizePolicy()
-        pol.setHorizontalPolicy(QtGui.QSizePolicy.Ignored)
-        pol.setVerticalPolicy(QtGui.QSizePolicy.Maximum)
+        pol = widgets.QSizePolicy()
+        pol.setHorizontalPolicy(widgets.QSizePolicy.Ignored)
+        pol.setVerticalPolicy(widgets.QSizePolicy.Maximum)
 
-        self.atoms_group = QtGui.QGroupBox('sample composition')
+        self.atoms_group = widgets.QGroupBox('sample composition')
         self.atoms_group.setSizePolicy(pol)
-        self.atoms_group_layout = QtGui.QHBoxLayout()
+        self.atoms_group_layout = widgets.QHBoxLayout()
         self.atoms_group_layout.addWidget(self.atoms_input)
         self.atoms_group.setLayout(self.atoms_group_layout)
 
-        self.parameters_group = QtGui.QGroupBox('interference parameters')
+        self.parameters_group = widgets.QGroupBox('interference parameters')
         self.parameters_group.setSizePolicy(pol)
-        self.parameters_group_layout = QtGui.QVBoxLayout()
+        self.parameters_group_layout = widgets.QVBoxLayout()
         self.parameters_group.setLayout(self.parameters_group_layout)
 
-        self.param_top_layout = QtGui.QGridLayout()
+        self.param_top_layout = widgets.QGridLayout()
         self.param_top_layout.addWidget(self.mz_label, 0, 0)
         self.param_top_layout.addWidget(self.mz_input, 1, 0)
         self.param_top_layout.addWidget(self.mzrange_label, 1, 1)
         self.param_top_layout.addWidget(self.mzrange_input, 1, 2)
 
-        self.param_bottom_layout = QtGui.QGridLayout()
+        self.param_bottom_layout = widgets.QGridLayout()
         self.param_bottom_layout.addWidget(self.charges_label, 0, 0)
         self.param_bottom_layout.addWidget(self.charges_input, 1, 0)
         self.param_bottom_layout.addWidget(self.chargesign_input, 1, 1)
@@ -186,16 +266,16 @@ class MainWidget(QtGui.QWidget):
         self.parameters_group_layout.addLayout(self.param_top_layout)
         self.parameters_group_layout.addLayout(self.param_bottom_layout)
 
-        self.button_layout = QtGui.QHBoxLayout()
+        self.button_layout = widgets.QHBoxLayout()
         self.button_layout.addWidget(self.interference_button)
         self.button_layout.addWidget(self.isotopes_button)
 
-        self.output_layout = QtGui.QHBoxLayout()
+        self.output_layout = widgets.QHBoxLayout()
         self.output_layout.addWidget(self.table_output)
 
-        self.layout = QtGui.QVBoxLayout()
+        self.layout = widgets.QVBoxLayout()
         self.layout.addWidget(self.atoms_group)
-        self.layout.addItem(QtGui.QSpacerItem(10,10))
+        self.layout.addItem(widgets.QSpacerItem(10,10))
         self.layout.addWidget(self.parameters_group)
         self.layout.addLayout(self.button_layout)
         self.layout.addLayout(self.output_layout)
@@ -289,7 +369,6 @@ class MainWidget(QtGui.QWidget):
         """ Validate input for atoms_input.
             Returns True on proper validation, False on error.
         """
-        _isotope_rx = r'(\d*[A-Z][a-z]{0,2})'
         atoms = re.findall(_isotope_rx, self.atoms_input.text())
         if not atoms:
             msg = 'Enter at least one element or isotope.'
@@ -308,7 +387,6 @@ class MainWidget(QtGui.QWidget):
         """ Validate input for charges_input.
             Returns True on correct input, False on error.
         """
-        _charges_rx = r'(\d+)'
         charges = re.findall(_charges_rx, self.charges_input.text())
         if not charges:
             msg = 'Enter at least one charge value.'
@@ -364,24 +442,9 @@ class MainWidget(QtGui.QWidget):
         else:
             super().keyPressEvent(event)
 
-    def format_float1f(self, num):
-        return '{:.1f}'.format(num)
-
-    def format_float5f(self, num):
-        return '{:.5f}'.format(num)
-
-    def format_float4g(self, num):
-        return '{:.4g}'.format(num)
-
-    def format_float6g(self, num):
-        return '{:.6g}'.format(num)
-
     @QtCore.pyqtSlot()
     def calculate(self):
         """ Take input, calculate mass spectrum, display in table. """
-        # Clear table
-        self.table_output.setHtml('')
-
         if not (self.check_atoms_input() and
                 self.check_charges_input() and
                 self.check_mz_input()):
@@ -395,65 +458,37 @@ class MainWidget(QtGui.QWidget):
         self.chargesign = self.chargesign_input.currentText()
         self.mzrange = self.mzrange_input.value()
 
-        # Make sure molecules column in DataFrame is not elided.
-        # DataFrame.to_html() apparently adheres to max_colwidth.
-        set_option('max_colwidth', 1024)
-
         ms = masstable.mass_interference(self.atoms, self.mz, mzrange=self.mzrange, 
-                maxsize=self.maxsize, charge=self.charges, chargesign=self.chargesign,
-                minabundance=self.minabundance)
-
-        # Don't need charge column
+            maxsize=self.maxsize, charge=self.charges, chargesign=self.chargesign,
+            minabundance=self.minabundance)
         ms.pop('charge')
-
-        # Nicer labels
         ms.columns = ['molecule', 'mass/charge', 'Δmass/charge', 'mz/Δmz (MRP)', 'probability']
+        ms.index = range(1, ms.shape[0] + 1)
 
-        table = ms.to_html(escape=False, index=False, classes='sortable',
-                           justify='left',
-                           formatters={'mass/charge': self.format_float5f,
-                                       'Δmass/charge': self.format_float5f,
-                                       'mz/Δmz (MRP)': self.format_float1f,
-                                       'probability': self.format_float6g})
-
-        # to_html sets border=1 with no option to turn it off
-        table = re.sub('border=\"1\" ', '', table)
-
-        self.table_output.setHtml(sorttable.html_head + table + sorttable.html_foot)
+        model = TableModel(ms)
+        self.table_output.setModel(model)
+        self.table_output.horizontalHeader().setSectionResizeMode(widgets.QHeaderView.Stretch)
 
     @QtCore.pyqtSlot()
     def show_isotopes(self):
         """ Show the standard ratios. """
-        # Clear table
-        self.table_output.setHtml('')
-
         if not self.check_atoms_input():
             return
 
-        # Make sure molecules column in DataFrame is not elided.
-        # DataFrame.to_html() apparently adheres to max_colwidth.
-        set_option('max_colwidth', 1024)
+        d = masstable.standard_ratios(self.atoms)
+        # recreate DataFrame to get index (isotopes) as a column.
+        ratios = DataFrame([d.index, d['isotope_mass'], d['abundance'], d['inverse']]).T
+        ratios.columns = ['isotope', 'mass', 'abundance', 'inverse']
+        ratios.index = range(1, ratios.shape[0] + 1)
 
-        ratios = masstable.standard_ratios(self.atoms)
-
-        # Nicer labels
-        ratios.columns = ['mass', 'abundance', 'inverse']
-
-        table = ratios.to_html(escape=False, index=True, classes='sortable',
-                           justify='left', index_names=True,
-                           formatters={'mass': self.format_float5f,
-                                       'abundance': self.format_float6g,
-                                       'inverse': self.format_float1f})
-
-        # to_html sets border=1 with no option to turn it off
-        table = re.sub('border=\"1\" ', '', table)
-
-        self.table_output.setHtml(sorttable.html_head + table + sorttable.html_foot)
+        model = TableModel(ratios)
+        self.table_output.setModel(model)
+        self.table_output.horizontalHeader().setSectionResizeMode(widgets.QHeaderView.Stretch)
 
 
 def run():
     """ Run the gui. """
-    app = QtGui.QApplication([])
+    app = widgets.QApplication([])
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
