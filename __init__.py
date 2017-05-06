58b1767b13f3132e69e9c2c8ecc729e6da253491
zanpeeters <zan@asiaa.sinica.edu.tw>
2017-05-06 17:29:34 +0800

    Change import to reflect name changes.

--- __init__.py
+++ __init__.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-""" Load the masstable module.
-    
-    Masstable uses a local database to store isotope data fetched from
-    NIST and other wedsites. This file is stored in the same directory
+""" Load the interference_calculator module.
+
+    IsotopeData uses a local database to store isotope data fetched from
+    NIST and other websites. This file is stored in the same directory
     as where this module is installed. The installation path may require
     admin rights for write access. If you wish to update the database, or
     create one on first installation of this module, run the following
     as root/Administrator/God:
-    
-    on first install
-    $ sudo python -M masstable -c "masstable.MassTable()"
-    
-    to update
-    $ sudo python -M masstable -c "mt = masstable.MassTable(); mt.update()"
+
+    $ sudo python -M interference_calculator.isotope_data -c \
+         "interference_calculator.isotope_data.update()"
 """
 from __future__ import absolute_import
-from .masstable import *
+from .isotope_data import *
+from .molecule import *
+from .interference_calculator import *
