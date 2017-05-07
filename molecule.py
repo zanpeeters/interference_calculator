69a24b442806b2c97e1fbc2d0f5db7a611634000
zanpeeters <zan@asiaa.sinica.edu.tw>
2017-05-08 02:47:56 +0800

    Update Molecule to work with new and improved periodic table; also adjust total mass of molecule by adding/subtracting electron mass for charged ions.

--- molecule.py
+++ molecule.py
@@ -5,14 +5,18 @@
     The chemical formula can be output in a number of ways, including custom
     formatting using simple templates.
 """
-import interference_calculator as ic
+import pandas as pd
 from collections import Counter
 from numpy import prod
-from math import factorial
-# from scipy.misc import factorial
+from scipy.misc import factorial
 import re
 
 __all__ = ['Molecule', 'templates', 'html_template', 'latex_template', 'isotope_template']
+
+periodic_table = pd.read_csv('periodic_table.csv', comment='#')
+
+# CODATA 2014, http://physics.nist.gov/cgi-bin/cuu/Value?me
+mass_electron = 0.0005485799090
 
 # How to determine the input notation?
 # If it contains a separation marker (space or anything not letter, numbers, or []+-),
@@ -28,7 +32,7 @@ templates = ['html_template', 'latex_template', 'isotope_template']
 html_template = {
     'isotope': '<sup>{}</sup>',
     'element': '{}',
-    'stoich': '<sub>{}</sub>',
+    'count': '<sub>{}</sub>',
     'charge': '<sup>{}</sup>',
     'minorjoin': '',
     'majorjoin': ''
@@ -37,7 +41,7 @@ html_template = {
 latex_template = {
     'isotope': '{{}}^{{{}}}',
     'element': '{{{}}}',
-    'stoich': '_{{{}}}',
+    'count': '_{{{}}}',
     'charge': '{{}}^{{{}}}',
     'minorjoin': '',
     'majorjoin': ''
@@ -46,7 +50,7 @@ latex_template = {
 isotope_template = {
     'isotope': '{}',
     'element': '{}',
-    'stoich': '{}',
+    'count': '{}',
     'charge': '{}',
     'minorjoin': '',
     'majorjoin': ' '
@@ -69,7 +73,7 @@ class Molecule(object):
 
             Isotope notation is a separated list of elements, where each
             element is an isotope of the form NXxxn, with N the isotope
-            number, Xxx is the element name, and n is the stoichiometric
+            number, Xxx is the element name, and n is the count
             number. Any character except A-Z, 0-9, +, -, [, or ] may be
             used to separate the elements. If no isotope number is specified,
             the most common isotope is assumed (e.g. C -> 12C). A charge
@@ -79,7 +83,7 @@ class Molecule(object):
             Isotope notation: '12C2 15N O3 2-'
 
             Emperical formula notation is a form of shorthand. It contains
-            no spaces and no isotope numbers, only stoichiometric numbers.
+            no spaces and no isotope numbers, only count numbers.
             Isotopes can optionally be given, surrounded by []. A charge
             may optionally be given at the end, also surrounded by [].
             This form is useful for inputting larger molecules with few
@@ -87,14 +91,13 @@ class Molecule(object):
 
             Empirical formula notation: 'HCOOCH2[15]NH3[2-]'
 
-            D and T are accepted as aliases for 2H and 3H, but are internally
-            converted to 2H and 3H. See Molecule.formula() for output options.
+            D is an accepted alias for 2H, but is internally converted
+            to 2H. See Molecule.formula() for output options.
 
-            After parsing the input formula, the compositional information
-            is stored under Molecule.composition as a pandas.DataFrame. This
-            is a slice (copy) of the MassTable, with an extra column containing
-            the stoichiometry. Other information stored in the Molecule class
-            is Molecule.mass, Molecule.charge, and Molecule.chargesign.
+            After parsing, relevant information about the molecule, such as
+            total mass, abundance, charge, and sign, as well as lists of
+            atoms, isotopes, atomic masses, and a few others are stored in
+            the Molecule() object.
         """
         self.input = molecule
         self.mass = 0.0
@@ -102,7 +105,14 @@ class Molecule(object):
         self.charge = 0
         self.chargesign = ''
 
+        self.elements = []
+        self.atomic_numbers = []
+        self.atomic_masses = []
+        self.masses = []
+        self.abundances = []
+
         self.parse(molecule)
+        self.relative_abundance()
 
     def __str__(self):
         return self.input + ' --> ' + self.formula()
@@ -157,8 +167,7 @@ class Molecule(object):
 
             # C -> 12C
             if u[0] == '':
-                idx = ic.elements.index(u[1])
-                mi = ic.main_isotopes[idx]
+                mi = periodic_table[periodic_table['element'] == u[1]].iloc[0].loc['major isotope']
                 u[0] = mi.strip(u[1])
 
             # Stoichiometry
@@ -172,29 +181,80 @@ class Molecule(object):
                     raise FormatError(msg.format(''.join(u)))
 
             # Isotope from element and isotope number.
-            # Repeat each isotope stoich. times, so Counter can count all.
+            # Repeat each isotope count times, so Counter can count all.
             iunits += [str(u[0]) + u[1]] * u[2]
 
         c = Counter(iunits)
-        self.isotopes = list(c.keys())
-        # TODO: make Molecule sortable, use self.sort()
-        self.isotopes.sort()
-        self.isotopes = tuple(self.isotopes)
+        self.isotopes = sorted(c.keys())
+        self.count = [c[i] for i in self.isotopes]
 
-        # Don't use c.values, isot/keys are sorted
-        self.stoichiometry = tuple(c[i] for i in self.isotopes)
-        self.indices = tuple(ic.isotopes.index(i) for i in self.isotopes)
-        self.elements = tuple(ic.elements[i] for i in self.indices)
-        self.atomic_numbers = tuple(ic.atomic_numbers[i] for i in self.indices)
-        self.unit_masses = tuple(ic.unit_masses[i] for i in self.indices)
-        self.isotope_masses = tuple(ic.isotope_masses[i] for i in self.indices)
-        self.abundances = tuple(ic.abundances[i] for i in self.indices)
+        for i in self.isotopes:
+            isotope = periodic_table[periodic_table['isotope'] == i].iloc[0]
+            self.elements.append(isotope['element'])
+            self.atomic_numbers.append(isotope['atomic number'])
+            self.atomic_masses.append(isotope['atomic mass'])
+            self.masses.append(isotope['mass'])
+            self.abundances.append(isotope['abundance'])
 
-        for m, s in zip(self.isotope_masses, self.stoichiometry):
+        for m, s in zip(self.masses, self.count):
             self.mass += m * s
 
-        # call abun func here
+        # adjust mass for extra or missing electrons (charge)
+        if self.chargesign == '+':
+            self.mass -= mass_electron * self.charge
+        elif self.chargesign == '-':
+            self.mass += mass_electron * self.charge
 
+    def relative_abundance(self):
+        """ Given a list of isotopes and a list of count numbers
+            calculate relative abundance for entire molecule.
+        """
+        # multiple isotopes e.g. 28Si (92.2%) 29Si (4.7%) 30Si (3.1%)
+        # In this type of mass spectorscopy we only look at total mass of molecule,
+        # not position of isotope. Therefore Si4-29Si has 5 isobaric structures:
+        #   Si4-29Si, Si3-29Si-Si, Si2-29Si-Si2, Si-29Si-Si3, 29Si-Si4
+        #
+        # Same problem as drawing 3 green + 2 red balls from a bag of multi-coloured
+        # balls. Calculate the probability mass function of multinomial distribution
+        # with replacement. Replacement condition is acceptable, since pool from
+        # which isotopes are pulled from which molecule is made (nature, sample
+        # in mass spectrometer during sputtering) is large compared to number of
+        # molecules being formed and therefore composition of pool does not change
+        # significantly (assuming homogenous distribution).
+        #
+        #   f(xi, n, pi) = n!/(x1!*x2!*...xk!) * (p1**x1 * p2**x2 * ... pk**xk)
+        #   for i = 1..k
+        # with
+        #   n = total number of all isotopes from the same parent element
+        #     = sum(xi) for i=1..k
+        #   k = number of different isotopes in molecule
+        #   xi = number of isotope i = count
+        #   pi = probability of isotope i = natural abundance
+        #
+        # Example: molecule 12C 16O2 18O
+        # C is independent of O
+        # there are 3 O in the molecule, n = 3
+        # there are 2 O isotopes in this molecule, k = 2
+        # for 16O: xi = 2, for 18O: xi = 1
+        # for 16O: pi = 0.9976 for 18O: pi = 0.002 (and 0.0004 for 17O)
+
+        data = periodic_table[periodic_table['isotope'].isin(self.isotopes)].copy()
+        data['count'] = self.count
+
+        parents = data['major isotope'].value_counts().to_dict()
+        abun_per_el = []
+        for el, k in parents.items():
+            d = data[data['major isotope'] == el]
+            n = d['count'].sum()
+
+            if k == 1:
+                # Simple case of single isotope, even if it occurs n times
+                abun = d['abundance'].iat[0] ** n
+            else:
+                abun = factorial(n)/factorial(d['count']).prod() * (d['abundance'] ** d['count']).prod()
+
+            abun_per_el.append(abun)
+        self.abundance = prod(abun_per_el)
 
     def sort(self):
         """ Sort elements from heavy to light, but keep same elements together. """
@@ -217,9 +277,9 @@ class Molecule(object):
 
             If style='custom', a custom template can be used to
             format the molecular formula. The template must be
-            a dict containing 6 keys: isotope, element, stoich,
+            a dict containing 6 keys: isotope, element, count,
             charge, minorjoin, and majorjoin. The isotope,
-            element, stoich, and charge keys should refer to a
+            element, count, and charge keys should refer to a
             template string containing a curly bracket pair,
             which will be replaced using string.format(). The
             minorjoin string will be used to connect all the
@@ -237,21 +297,21 @@ class Molecule(object):
         else:
             charge = ''
 
-        elem = list(self.elements)
-        umass = [str(u) for u in self.unit_masses]
-        stoich = [str(s) if s > 1 else '' for s in self.stoichiometry]
+        elem = self.elements
+        amass = [str(u) for u in self.atomic_masses]
+        count = [str(c) if c > 1 else '' for c in self.count]
 
         if HtoD:
-            for n, x in enumerate(zip(umass, elem)):
-                um, el = x
+            for n, x in enumerate(zip(amass, elem)):
+                am, el = x
                 if el == 'H':
-                    if um == '1':
-                        umass[n] = ''
-                    elif um == '2':
-                        umass[n] = ''
+                    if am == '1':
+                        amass[n] = ''
+                    elif am == '2':
+                        amass[n] = ''
                         elem[n] = 'D'
-                    elif um == '3':
-                        umass[n] = ''
+                    elif am == '3':
+                        amass[n] = ''
                         elem[n] = 'T'
 
         if style == 'html':
@@ -266,72 +326,24 @@ class Molecule(object):
             templ = isotope_template
 
         molecule = []
-        for um, el, st in zip(umass, elem, stoich):
-            if um:
-                um_str = templ['isotope'].format(um)
+        for am, el, ct in zip(amass, elem, count):
+            if am:
+                am_str = templ['isotope'].format(am)
             else:
-                um_str = ''
+                am_str = ''
             el_str = templ['element'].format(el)
-            if st:
-                st_str = templ['stoich'].format(st)
+            if ct:
+                ct_str = templ['count'].format(ct)
             else:
-                st_str = ''
-            m = templ['minorjoin'].join((um_str, el_str, st_str))
+                ct_str = ''
+            m = templ['minorjoin'].join((am_str, el_str, ct_str))
             molecule.append(m)
         if charge:
             molecule.append(templ['charge'].format(charge))
         return templ['majorjoin'].join(molecule)
 
-    def relative_abundance(isotopes, stoichometry):
-        """ Given a list of isotopes and a list of stoichiometric numbers
-            calculate relative abundance for entire molecule. """
-        # multiple isotopes e.g. 28Si (92.2%) 29Si (4.7%) 30Si (3.1%)
-        # In this type of mass spectorscopy we only look at total mass of molecule,
-        # not position of isotope. Therefore Si4-29Si has 5 isobaric structures:
-        #   Si4-29Si, Si3-29Si-Si, Si2-29Si-Si2, Si-29Si-Si3, 29Si-Si4
-        #
-        # Same problem as drawing 3 green + 2 red balls from a bag of multi-coloured
-        # balls. Calculate the probability mass function of multinomial distribution
-        # with replacement. Replacement condition is acceptable, since pool from
-        # which isotopes are pulled from which molecule is made (nature, sample
-        # in mass spectrometer during sputtering) is large compared to number of 
-        # molecules being formed and therefore composition of pool does not change
-        # significantly (assuming homogenous distribution).
-        # 
-        #   f(xi, n, pi) = n!/(x1!*x2!*...xk!) * (p1**x1 * p2**x2 * ... pk**xk)
-        #   for i = 1..k
-        # with
-        #   n = total number of all isotopes from the same parent element
-        #     = sum(xi) for i=1..k
-        #   k = number of different isotopes in molecule
-        #   xi = number of isotope i = stoichiometry
-        #   pi = probability of isotope i = natural abundance
-        #
-        # Example: molecule 12C 16O2 18O
-        # C is independent of O
-        # there are 3 O in the molecule, n = 3
-        # there are 2 O isotopes in this molecule, k = 2
-        # for 16O: xi = 2, for 18O: xi = 1
-        # for 16O: pi = 0.9976 for 18O: pi = 0.002 (and 0.0004 for 17O)
-
-        data = ic.isotope_data.loc[isotopes]
-        data['stoich'] = stoichiometry
-
-        parents = data['main_isotope'].value_counts().to_dict()
-        abun_per_el = []
-        for el, k in parents.items():
-            d = data[data['main_isotope'] == el]
-            n = d['stoich'].sum()
-
-            if k == 1:
-                # Simple case of single isotope, even if it occurs n times
-                abun = d['abundance'].iat[0] ** n
-            else:
-                abun = factorial(n)/factorial(d['stoich']).prod() * (d['abundance'] ** d['stoich']).prod()
-
-            abun_per_el.append(abun)
-        return prod(abun_per_el)
-
 
 if __name__ == '__main__':
-    print(Molecule('12C2 15N H4 2+').formula(style='html'))
+    m = Molecule('12C2 15N H4 2+')
+    print(m.formula(style='html'))
+    print(m.mass, m.abundance)
