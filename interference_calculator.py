3769753b253809631c26186ba552590f322d656e
zanpeeters <zan@asiaa.sinica.edu.tw>
2017-05-08 15:01:34 +0800

    Overhaul interference_calculator to work with revamped Molecule and periodic table. Simplify calculation and make use of binomial calculation of probability (Molecule.relative_abundance()).

--- interference_calculator.py
+++ interference_calculator.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """ Calculate isotopic interference and standard ratios. """
-import interference_calculator as ic
-from pandas import concat
+
+import pandas as pd
+import itertools
+from molecule import Molecule, mass_electron
 
 __all__ = ['interference', 'standard_ratio']
 
+periodic_table = pd.read_csv('periodic_table.csv', comment='#')
+
 def interference(atoms, mz, mzrange=0.3, maxsize=5, charge=[1],
-                chargesign='-', formula_style='html'):
+                 chargesign='-', style='html'):
     """ For a list of atoms, calculate all molecular ions that can be formed
         from those atoms, including all isotopes, up to maxsize atoms,
         that have a mass-to-charge ratio within mz +/- mzrange.
@@ -23,22 +27,18 @@ def interference(atoms, mz, mzrange=0.3, maxsize=5, charge=[1],
         to also include higher charged ions. Masses are adjusted for missing
         electrons (+ charge) or extra electrons (- charge).
 
-        Molecular formulas are represented in the style in formula_style (default
-        is 'html'). See Molecule() for more options.
+        Molecular formulas are formatted in style (default is 'html').
+        See Molecule() for more options.
 
         Returns a pandas.DataFrame with a column 'molecule' with molecular formula,
-        a column 'mass/charge' for the mass-to-charge ratio, a column 'mass/charge diff'
-        for the mass/charge difference between this ion and the target ion, a column
-        'MRP' which gives the mass-resolving power (mz/Δmz) needed to resolve the
-        target ion from this ion, a column 'charge', and a column 'probability',
-        which for now is simply the product of the abundances of the isotopes in
-        the given molecular ion.
+        a column 'charge', a column 'mass/charge' for the mass-to-charge ratio, a
+        column 'mass/charge diff' for the mass/charge difference between this ion
+        and the target mass/charge, a column 'MRP' which gives the mass-resolving
+        power (mz/Δmz) needed to resolve this ion from the target ion, and a column
+        'probability', which gives the combinatorial probability of encoutering this
+        combination of isotopes, given the composition of the sample.
     """
-    picked_atoms = []
-    for a in atoms:
-        picked_atoms.append(ic.isotope_data[ic.isotope_data.element == a])
-
-    picked_atoms = concat(picked_atoms).query('index not in ("H", "D", "T")')
+    picked_atoms = periodic_table[periodic_table['element'].isin(atoms)]
 
     # Mass-to-charge can be given as either a number, or as a molecule (string).
     # Calculate m/z from molecular formula.
@@ -46,132 +46,99 @@ def interference(atoms, mz, mzrange=0.3, maxsize=5, charge=[1],
         try:
             mz = float(mz)
         except ValueError:
-            molec = ic.Molecule(mz)
-            ch = molec.charge
+            m = Molecule(mz)
+            ch = m.charge
             if ch == 0:
                 ch = 1
-            mz = molec.mass / ch
+            mz = m.mass / ch
 
-    # Create a list with all possible combinations up to maxsize atoms
-    # Create same list for masses and abundances; combos are created in same order.
+    # Create a list with all possible combinations up to maxsize atoms.
+    # Create same list for masses, combos are created in same order.
     isotope_combos = []
     mass_combos = []
-    abundance_combos = []
     for size in range(1, maxsize + 1):
-        i = itertools.combinations_with_replacement(picked_atoms.index, size)
-        m = itertools.combinations_with_replacement(picked_atoms.isotope_mass, size)
-        a = itertools.combinations_with_replacement(picked_atoms.abundance, size)
-        isotope_combos += list(i)
-        mass_combos += list(m)
-        abundance_combos += list(a)
+        i = itertools.combinations_with_replacement(picked_atoms['isotope'], size)
+        m = itertools.combinations_with_replacement(picked_atoms['mass'], size)
+        isotope_combos.extend(list(i))
+        mass_combos.extend(list(m))
 
-    # Calculate total mass (sum), probability (product, below)
-    masses = DataFrame(mass_combos).sum(axis=1)
-    abundances = DataFrame(abundance_combos)
+    masses = pd.DataFrame(mass_combos).sum(axis=1)
 
-    # There can be missing values because: (A) missing from NIST
-    # table, (B) because combos were of different length, and turning
-    # them into a square array fills up each row with NaNs.
-    # Replaced A-type NaN with -1 or minabundance earlier, replace
-    # B-type NaN with 1 here (will be eaten by product), then
-    # -1 back to NaN (nothing should be < 0 if minabundance was given).
-    print(abundances)
-    # abundances = abundances.fillna(value=1)
-    # abundances = abundances.where(abundances >= 0)
-    abundances = abundances.prod(axis=1, skipna=False)
-    print(abundances)
-
-    # Using Molecule() to convert atom list to molecular formula
-    # is too slow for long list. Simple join for now.
+    # Using Molecule() to convert atom list to molecular formula and to
+    # calculate abundance is too slow for long list.
+    # For 5 atoms, max size 5, 8567 combos: 80 s. Do for trimmed list later.
     molecules = [' '.join(m) for m in isotope_combos]
 
-    data = DataFrame({'molecule': molecules,
-                      'mass/charge': masses,
-                      'probability': abundances})
+    data = pd.DataFrame({'molecule': molecules,
+                         'mass/charge': masses})
 
-    if len(charge) == 1:
-        if charge[0] == 1:
-            charge_str = [''] * len(data.index)
+    data_w_charge = []
+    for ch in charge:
+        if ch == 1:
+            charge_str = ' [{}]'.format(chargesign)
         else:
-            charge_str = [str(charge[0])] * len(data.index)
-        data['charge'] = charge[0]
-    else:
-        cc = []
-        charge_str = []
-        for c in charge:
-            cc.extend([c] * len(data.index))
-            if c == 1:
-                charge_str.extend([''] * len(data.index))
-            else:
-                charge_str.extend([str(c)] * len(data.index))
+            charge_str = ' [{}{}]'.format(ch, chargesign)
+        d = data.copy()
+        d['charge'] = ch
+        d['mass/charge'] /= ch
+        d['molecule'] += charge_str
+        data_w_charge.append(d)
+    data = pd.concat(data_w_charge)
 
-        # repeat data once for each charge
-        data = concat([data] * len(charge))
-        data['charge'] = cc
-
-    charge_str = [' [' + c + chargesign + ']' for c in charge_str]
-    data['molecule'] += charge_str
-
-    # Correct mass for charge * electron mass
+    # Correct mass for extra/missing electrons.
+    # Only once, mass already divided by charge.
     if chargesign == '+':
-        data['mass/charge'] -= data['charge'] * mass_electron
+        data['mass/charge'] -= mass_electron
     elif chargesign == '-':
-        data['mass/charge'] += data['charge'] * mass_electron
+        data['mass/charge'] += mass_electron
     else:
         raise ValueError('chargesign must be either "+" or "-".')
 
-    data['mass/charge'] /= data['charge']
     if mz:
         data['mass/charge diff'] = data['mass/charge'] - mz
         data['MRP'] = mz/data['mass/charge diff'].abs()
     else:
-        data['mass/charge diff'] = 0
-        data['MRP'] = 0
+        data['mass/charge diff'] = 0.0
+        data['MRP'] = 0.0
 
     if mz:
-        results = data.loc[(data['mass/charge'] >= mz - mzrange)
-                         & (data['mass/charge'] <= mz + mzrange)]
-    else:
-        results = data
+        data = data.loc[(data['mass/charge'] >= mz - mzrange)
+                      & (data['mass/charge'] <= mz + mzrange)]
 
-    def convert(s):
-        m = ic.Molecule(s)
-        return m.formula(style=formula_style)
+    molec = []
+    abun = []
+    for molecule in data['molecule'].values:
+        m = Molecule(molecule)
+        abun.append(m.abundance)
+        molec.append(m.formula(style=style))
 
-    # Next line gives SettingWithCopyWarning no matter how I do it.
-    results.loc[:, 'molecule'] = results['molecule'].apply(convert)
-    results = results.ix[:, ['molecule',
-                             'charge',
-                             'mass/charge',
-                             'mass/charge diff',
-                             'MRP',
-                             'probability']]
+    data['molecule'] = molec
+    data['probability'] = abun
+    return data[['molecule', 'charge', 'mass/charge',
+                 'mass/charge diff', 'MRP', 'probability']]
 
-    return results.sort_values('mass/charge')
-
-
-def standard_ratio(atoms, formula_style='html'):
-    """ Give the stable isotope and solar abundance for the given element(s). """
-    data = []
+def standard_ratio(atoms, style='html'):
+    """ Give the stable isotopes and their standard abundance for the given element(s). """
+    data = periodic_table[periodic_table['element'].isin(atoms)].copy()
+    data['ratio'] = 1.0
+    data['inverse ratio'] = 1.0
     for a in atoms:
-        eldata = ic.isotope_data[ic.isotope_data['element'] == a]
-        highest = eldata['abundance'].max()
-        eldata['inverse'] = highest/eldata['abundance']
-        data.append(eldata)
+        abun = data.loc[data['element'] == a, 'abundance'].copy()
+        ratio = abun/abun.max()
+        inv_ratio = 1/ratio
+        data.loc[data['element'] == a, 'ratio'] = ratio
+        data.loc[data['element'] == a, 'inverse ratio'] = inv_ratio
 
-    results = concat(data)
+    pretty_isotopes = []
+    for i in data['isotope'].values:
+        m = Molecule(i)
+        pretty_isotopes.append(m.formula(style=style, show_charge=False))
+    data['isotope'] = pretty_isotopes
 
-    def convert(s):
-        m = ic.Molecule(s)
-        return m.formula(style=formula_style, show_charge=False)
-
-    results.index = [convert(i) for i in results.index]
-    results = results.ix[:, ['isotope_mass', 'abundance', 'inverse']]
-
-    return results
+    return data[['isotope', 'mass', 'abundance', 'ratio', 'inverse ratio', 'standard']]
 
 if __name__ == '__main__':
     print('Possible interferences for MgO given as sample consisting of H, C, O, N, and Si:')
     print(interference(['H', 'C', 'O', 'N', 'Si'], 'MgO'))
     print('Standard ratios of C and S:')
-    print(standard_ratios(['C', 'S']))
+    print(standard_ratio(['C', 'S']))
