febb51c259f3a38c2082465996755062571144d6
zanpeeters <zan@asiaa.sinica.edu.tw>
2017-11-21 00:08:45 -0700

    Delete old information from __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-""" Load the interference_calculator module.
-
-    IsotopeData uses a local database to store isotope data fetched from
-    NIST and other websites. This file is stored in the same directory
-    as where this module is installed. The installation path may require
-    admin rights for write access. If you wish to update the database, or
-    create one on first installation of this module, run the following
-    as root/Administrator/God:
-
-    $ sudo python -M interference_calculator.isotope_data -c \
-         "interference_calculator.isotope_data.update()"
-"""
+# -*- coding: utf-8 -*-
+""" Load the interference_calculator module. """
 from .molecule import *
 from .interference_calculator import *
