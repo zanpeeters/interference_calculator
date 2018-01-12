84a6b81721475878e1ec09f9749aec5d75484faf
zanpeeters <zan.peeters@asu.edu>
2018-01-12 15:35:41 -0700

    Use separate file for version.

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@
 """ Load the interference_calculator module. """
 from interference_calculator.molecule import *
 from interference_calculator.main import *
+from interference_calculator.version import __version__
