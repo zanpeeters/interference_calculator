a7fd7b017c2438b14ad9b6fd13542246ede8820f
zanpeeters <zan.peeters@asu.edu>
2018-01-16 10:49:47 -0700

    Merge version.py into __init__.py and expand metadata.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
-""" Load the interference_calculator module. """
-from interference_calculator.molecule import *
-from interference_calculator.main import *
-from interference_calculator.version import __version__
+""" Calculate mass interference and standard isotopic ratios for mass spectrometry. """
+from interference_calculator.molecule import Molecule, periodic_table, mass_electron, templates
+from interference_calculator.main import interference, standard_ratio
+
+__version__ = '1.0.0'
+__name__ = 'interference_calculator'
+__author__ = 'Zan Peeters'
+__url__ = 'https://github.com/zanpeeters/interference_calculator'
+__license__ = 'BSD 3-Clause Clear'
+__copyright__ = '(c) 2017 Zan Peeters'
+__description__ = __doc__
