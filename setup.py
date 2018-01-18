a6c32811da8a42426aa72e1fa349419524d2699e
zanpeeters <zan.peeters@asu.edu>
2018-01-18 09:57:50 -0700

    Create packaging files for sdist and bdist_wheel.

--- setup.py
+++ setup.py
@@ -1,37 +1,69 @@
 #!/usr/bin/env python
-""" Distutils setup file for interference calculator. """
-from setuptools import setup
+""" Setuptools setup file for interference calculator. """
+from setuptools import setup, find_packages
+import os
 
-# from distutils.core import setup
-# import py2exe
-#
-# setup(console=['hello.py'])
+with open(os.path.join('interference_calculator', '__init__.py'), mode='rt', encoding='utf-8') as fh:
+    script = []
+    for l in fh.readlines():
+        l = l.strip()
+        # don't import anything, just get metadata
+        if l.startswith('from') or l.startswith('import'):
+            continue
+        script.append(l)
 
-# write test to check for either PyQt4 or PyQt5 (PySide not tested)
+exec('\n'.join(script))
 
+with open('README.md', mode='rt', encoding='utf-8') as fh:
+    __long_description__ = fh.read()
+
+try:
+    import PyQt5
+    pyqtdep = 'PyQt5'
+except ImportError:
+    try:
+        import PyQt4
+        pyqtdep = 'PyQt4'
+    except ImportError:
+        pyqtdep = 'PyQt5'
 
 setup(
-    # metadata
-    name = 'interference calculator',
-    version = '1.0',
-    description = 'A tool to calculate mass interference based on a target material and a target mass. Useful for mass spectroscopy (SIMS, TIMS, ICP-MS) of elements and small molecules and their isotopes. The program can also display the standard ratio of all isotopes based on a given standard.',
-    url = 'https://github.com/zanpeeters/interference_calculator',
-    author = 'Zan Peeters',
-    license = 'BSD 3-Clause Clear',
+    name = __name__,
+    version = __version__,
+    description = __description__,
+    long_description = __long_description__,
+    url = __url__,
+    author = __author__,
+    author_email = 'me@example.com',
+    license = __license__,
     classifiers = [
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Development Status :: 4 - Beta',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering'
     ],
-    keywords = 'interference, mass spectroscopy, isotope, element, standard ratio',
+    keywords = 'interference mass spectrometry isotope element standard ratio',
 
-    # package info
-    install_requires = ['pandas', 'PyQt4'],
-    package_data = {'': ['periodic_table.csv']}
+    install_requires = [
+        'matplotlib',
+        'pandas',
+        'pyparsing',
+        pyqtdep,
+        'scipy'
+    ],
     entry_points = {
-        'gui_scripts': ['interference_calculator = interference_calculator_ui:run']
-    }
+        'gui_scripts': ['interference_calculator=interference_calculator.ui:run']
+    },
+
+    packages = find_packages(),
+    package_data = {'interference_calculator': [
+        'periodic_table.csv',
+        'icon.svg',
+        'display_button_icon.svg',
+        'help_button_icon.svg']
+    },
+    zip_safe = False
 )
